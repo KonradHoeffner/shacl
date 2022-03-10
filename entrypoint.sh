09db7ff2f93ba37ed690e7d376fe3e383ec03d73
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-03-10 15:11:18 +0100

    Fix docker approach.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
-echo $shacl
-echo $data
+#!/bin/bash
+shacl=$1
+data=$2
 log=$(pyshacl -a -s ${shacl} ${data})
-echo $log
-#violations=$(grep -c 'Constraint Violation' <<< "$log")
-#if [[ "$violations" -gt 0 ]]; then echo "::warning ::$log"; fi
-#echo $log
-#echo "::set-output name=violations::$violations"
+violations=$(grep -c 'Constraint Violation' <<< "$log")
+if [[ "$violations" -eq 0 ]]
+then
+	echo "RDG graph ${data} fully conforms to SHACL shape ${shacl}"
+else
+	log="${log//$'\n'/'%0A'}"
+	echo "::warning ::$log"
+fi
+echo "::set-output name=violations::$violations"
